78df11306abefc5f2b7f289ce4d71bf965ba52f7
tewebdevelopers <tewebdevelopers@gmail.com>
2018-04-08 14:45:45 +0300

    Changes of blog with logins and registration

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,13 @@
-from django.conf.urls import include
-from django.conf.urls import url 
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
+ 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-	url(r'',include('blog.urls')),
+    url(r'^admin/', admin.site.urls),   
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'',include('blog.urls')),
+    url(r'^home/', include('home.urls')),
+    
 ]
