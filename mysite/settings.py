60028a1b0e71c9488c4d0c7f71842e3d5e2295b5
tewebdevelopers <tewebdevelopers@gmail.com>
2018-05-02 08:33:36 +0300

    changes to css,post_list,settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9xj2kyvdxsdco8dscky%u#^n^_!$to^4&grj!9daukcoq4f6%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','localhost']
 
